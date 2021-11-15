be775323f19668f2b8e77ae91889b6d6bc18c1fc
Anshul Hedau <anshul.hedau2001@gmail.com>
2021-11-16 01:11:43 +0530

    Color theme set

--- static/javascript/web.js
+++ static/javascript/web.js
@@ -1,7 +1,11 @@
-var f = document.getElementById("upload");
+var f = document.getElementById("customFile");
 
 function myFunction() {
   if (f.checkValidity()) {
     alert("File upload succesfull!");
+  }
+  else {
+    alert("Please select a file!");
+    return false;
   }
 }
